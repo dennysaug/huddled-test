a164493f7befc01d16751656d58f10405a4f18a0
Dennys Augustus <dennysaug@gmail.com>
2022-03-17 20:33:15 +0000

    update route

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
-Route::get('show', function() {
-   return view('index');
-});
